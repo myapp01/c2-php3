39c0f6f862a79bab705d93d725c416edf87294b6
myapp01 <thanawatpalaket9090@gmail.com>
2021-11-19 11:44:03 +0700

    initial commit

---
+++ php302.php
@@ -0,0 +1,12 @@
+<?php
+$x = 20;
+
+echo "<h1>PHP Assignment Operators</h1>";
+echo "<h1> x = $x <br /></h1>";
+echo "<hr color='red' size='4px' />";
+
+echo "x += : ",$x += 10,"<br />";//$x = $x + 10
+echo "x -= : ",$x -= 10,"<br />";//$x = $x - 10
+echo "x *= : ",$x *= 10,"<br />";//$x = $x * 10
+echo "x /= : ",$x /= 10,"<br />";//$x = $x / 10
+echo "x %= : ",$x %= 10,"<br />";//$x = $x % 10
